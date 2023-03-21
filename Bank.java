e3fdbdc1ea57da5762d06a998d9beb67f04a5b7d
sushil singh <126874257+Rajababu1029@users.noreply.github.com>
2023-03-21 12:13:27 +0530

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -8,16 +8,33 @@ public class Bank {
 		try {
 			Scanner scn =  new Scanner(System.in);
 			System.out.println("Enter the balance");
+						
+			
 			Double balance = scn.nextDouble();
 			bankAcc.setBalance(balance);
 			
 			System.out.println("Enter the Amount for Deposit");
-			int Damt = scn.nextInt();
+			double Damt = scn.nextDouble();
 			bankAcc.deposit(Damt);
+			System.out.println("Current Balance:"+bankAcc.getBalance());
+	
 			System.out.println("Enter the Amount for withdraw");
-			int Wamt = scn.nextInt();
+			double Wamt = scn.nextDouble();
 			bankAcc.withdraw(Wamt);
-			System.out.println("Current Balance:-"+bankAcc.getBalance());
+			
+			System.out.println("Current Balance:"+bankAcc.getBalance() + "after the withdrawl transaction" );
+			
+			System.out.println("Enter the principal amount");
+			double principal = scn.nextDouble();
+			System.out.println("Enter the rate of interest");
+			double interestrate = scn.nextDouble();
+			System.out.println("Enter the time period");
+			int time = scn.nextInt();
+			bankAcc.compoundInterest(principal, interestrate, time);
+			
+			System.out.println("Current Balance:"+bankAcc.getBalance());
+
+			
 		} catch (InputMismatchException e) {
 			System.out.println("Enter only number ");
 		   }
