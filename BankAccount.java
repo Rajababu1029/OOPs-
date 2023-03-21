97c479c6a8fe9e0cbdad0c433df924b6d4a3d6cd
sushil singh <126874257+Rajababu1029@users.noreply.github.com>
2023-03-21 12:15:25 +0530

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -1,12 +1,9 @@
-//Encapculation concept
-public class BankAccount {
+
+public class BankAccount implements RBIBank{
 private double balance;
 
-public void setBalance(double balance) throws  IllegalArgumentException {
-	if (balance == 0) 
-		throw new IllegalArgumentException("Do not enter zero ");
-	if (balance <0) 
-		throw new IllegalArgumentException("Do not pass negative value");
+public void setBalance(double balance){
+	validationCheck(balance);
 		this.balance = balance;
 }
 
@@ -14,21 +11,28 @@ public double getBalance() {
 	return balance;
 }
 
-public void deposit(int amt) throws IllegalArgumentException{
-if (amt==0) 
-	throw new IllegalArgumentException("Do not pass zero");
-if (amt<0) 
-	throw new IllegalArgumentException("Do not pass negative value");
+public void deposit(double amt){
+	validationCheck(amt);
 	
 	this.balance= balance+amt;
 }
-public void withdraw(int amt) {
+public void compoundInterest(double principal, double interestRate, int time) {
+	validationCheck(principal);
+	validationCheck(interestRate);
+	validationCheck(time);	
+	double CI = principal*Math.pow(1+interestRate/100, time)-principal;
+	System.out.println("Compound Interest: "+CI);
+}
+
+public void withdraw(double amt){
+	validationCheck(amt);
+	this.balance= balance-amt;
+}
+public void validationCheck(double amt) throws  IllegalArgumentException{
 	if (amt==0) 
 		throw new IllegalArgumentException("Do not pass zero");
 	if (amt<0) 
 		throw new IllegalArgumentException("Do not pass negative value");
-	
-	this.balance= balance-amt;
+ 	}
 }
 
-}
